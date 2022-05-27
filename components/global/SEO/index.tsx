b4c23f504600856005059d6c07222690868b4d8d
Ali Amori kadhim <ali@Alis-MBP.home>
2022-05-27 16:50:00 +0200

    update favicon

--- components/global/SEO/index.tsx
+++ components/global/SEO/index.tsx
@@ -28,7 +28,13 @@ export const SEO: React.FC<SEOProps> = ({ seo, schemaOrg }) => {
       <meta property="og:url" content={ogURL} />
       <meta property="og:image" content={SEOImage} />
       <meta name="theme-color" content="#ffd166" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicon_io/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/favicon_io/android-chrome-512x512.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+      <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+      <link rel="manifest" href="/favicon_io/site.webmanifest" />
       <Script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
