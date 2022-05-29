025b7eadcb680dcb92f956dea2d569d782c50708
Ali Amori kadhim <ali@Alis-MBP.home>
2022-05-29 10:57:30 +0200

    use new ci/cd approach

---
+++ tests/runner.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const remote = process.env.VERCEL_URL;
+
+if (remote) {
+  console.log(`Testing against remote url: ${remote}.`);
+} else {
+  console.log('Testing locally only.');
+}
