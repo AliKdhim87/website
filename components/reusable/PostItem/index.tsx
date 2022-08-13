753e13a42be135e36c91991d2b168fbe940b058a
Ali Amori Kadhim <ali@frameless.io>
2022-08-13 11:44:50 +0200

    build: update dependencies

--- components/reusable/PostItem/index.tsx
+++ components/reusable/PostItem/index.tsx
@@ -2,6 +2,10 @@ import classNames from 'classnames/bind';
 
 import styles from './index.module.scss';
 
+import { Children } from 'global';
+
 const css = classNames.bind(styles);
 
-export const PostItem: React.FC = ({ children }) => <div className={css('post-item')}>{children}</div>;
+export const PostItem: React.FC<{ children: Children }> = ({ children }) => (
+  <div className={css('post-item')}>{children}</div>
+);
