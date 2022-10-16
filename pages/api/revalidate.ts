b48a4e1269d9e953239763d5481ce7f4baf63197
Ali Amori Kadhim <ali@frameless.io>
2022-10-16 16:14:07 +0200

    upgrade nextjs

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,11 +1,13 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
-    await res.unstable_revalidate('/');
+    await res.revalidate(`/${req.query.slug}`);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
