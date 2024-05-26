c784e128eb79796f624e59df9227591cf140694c
Ali Amori Kadhim <ali@frameless.io>
2024-05-26 20:06:21 +0200

    feat: improve main component

--- components/global/Main/index.tsx
+++ components/global/Main/index.tsx
@@ -1,13 +1,28 @@
 import classNames from 'classnames/bind';
-
-import { Children } from 'global';
+import { type DetailedHTMLProps, forwardRef, type HTMLAttributes, PropsWithChildren, ForwardedRef } from 'react';
 
 import styles from './index.module.scss';
 
 const css = classNames.bind(styles);
-interface Props {
-  children: Children;
-}
-export const MainWrapper = ({ children }: Props) => <div className={css('main-wrapper')}>{children}</div>;
 
-export const Main = ({ children }: Props) => <main className={css('main')}>{children}</main>;
+type MainWrapperProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+type MainProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+
+export const MainWrapper = forwardRef(
+  ({ children, ...restProps }: PropsWithChildren<MainWrapperProps>, ref: ForwardedRef<HTMLDivElement>) => (
+    <div className={css('main-wrapper')} ref={ref} {...restProps}>
+      {children}
+    </div>
+  ),
+);
+
+export const Main = forwardRef(
+  ({ children, ...restProps }: PropsWithChildren<MainProps>, ref: ForwardedRef<HTMLElement>) => (
+    <main className={css('main')} ref={ref} {...restProps}>
+      {children}
+    </main>
+  ),
+);
+
+MainWrapper.displayName = 'MainWrapper';
+Main.displayName = 'Main';
