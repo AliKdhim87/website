9a17b562db5a490b6eccbbe80fd7effae04997d2
Ali Amori Kadhim <ali@frameless.io>
2024-05-26 19:17:56 +0200

    feat: improve Link component

--- components/reusable/Anchor/index.tsx
+++ components/reusable/Anchor/index.tsx
@@ -1,17 +1,27 @@
 import classNames from 'classnames/bind';
-import { DetailedHTMLProps, AnchorHTMLAttributes } from 'react';
+import { DetailedHTMLProps, AnchorHTMLAttributes, forwardRef, PropsWithChildren, ComponentType } from 'react';
 
 import styles from './Anchor.module.scss';
 
 const css = classNames.bind(styles);
 
-type AnchorProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+export interface AnchorProps extends DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+  Link?: ComponentType<any>;
+}
 
-export const Anchor = ({ children, className, ...props }: AnchorProps) => {
-  const classes = css(className, 'anchor');
-  return (
-    <a className={classes} {...props}>
-      {children}
-    </a>
-  );
+export const Anchor = forwardRef<HTMLAnchorElement, PropsWithChildren<AnchorProps>>(
+  ({ children, Link: Component = 'a', className, ...props }, ref) => {
+    const classes = css(className, 'anchor');
+    return (
+      <Component className={classes} ref={ref} {...props}>
+        {children}
+      </Component>
+    );
+  },
+);
+
+Anchor.displayName = 'Anchor';
+
+Anchor.defaultProps = {
+  Link: undefined,
 };
