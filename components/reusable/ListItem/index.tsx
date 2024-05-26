081048c86a6e701adcb6840219a2bb2949a50d7e
Ali Amori Kadhim <ali@frameless.io>
2024-05-26 17:22:40 +0200

    feat: create List & ListItem components

---
+++ components/reusable/ListItem/index.tsx
@@ -0,0 +1,16 @@
+import classnames from 'classnames/bind';
+import { ForwardedRef, HTMLAttributes, PropsWithChildren, forwardRef } from 'react';
+
+import styles from './styles.module.scss';
+
+type ListItemProps = HTMLAttributes<HTMLLIElement>;
+const cx = classnames.bind(styles);
+export const ListItem = forwardRef(
+  ({ children, ...restProps }: PropsWithChildren<ListItemProps>, ref: ForwardedRef<HTMLLIElement>) => (
+    <li ref={ref} className={cx('list-item')} {...restProps}>
+      {children}
+    </li>
+  ),
+);
+
+ListItem.displayName = 'ListItem';
