cc322a85fef92cae44ec3bc9595c58f5ffd1df7d
Ali Amori Kadhim <ali@frameless.io>
2024-06-02 12:19:19 +0200

    feat: create NavLink component

---
+++ components/global/Nav/NavLink/index.tsx
@@ -0,0 +1,16 @@
+import classnames from 'classnames/bind';
+import Link, { LinkProps } from 'next/link';
+import type { HTMLAttributes, PropsWithChildren } from 'react';
+import { forwardRef } from 'react';
+
+import styles from '../index.module.scss';
+
+const css = classnames.bind(styles);
+
+export interface NavLinkProps extends HTMLAttributes<HTMLAnchorElement>, LinkProps {}
+
+export const NavLink = forwardRef(({ children, ...restProps }: PropsWithChildren<NavLinkProps>) => (
+  <Link className={css('nav-link')} {...restProps}>
+    {children}
+  </Link>
+));
