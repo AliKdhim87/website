156cd2e439362a642dd54d3a0c85d424060fc5f2
Ali Amori Kadhim <ali@frameless.io>
2024-06-04 22:11:08 +0200

    feat: create useWindowSize hook

---
+++ utils/useWindowSize.ts
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export const useScreen = () => {
+  const isMobile = () => window.innerWidth < 768;
+
+  return { isMobile };
+};
+
+export const useWindowSize = (): { width?: number } => {
+  const isWindow = typeof window !== 'undefined';
+  const [width, setWidth] = useState(isWindow ? window.innerWidth : undefined);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(isWindow ? window.innerWidth : undefined);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isWindow]);
+
+  return { width };
+};
