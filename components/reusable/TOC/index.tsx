a9012da4e9f3c890cb4698635659b3f3ab849507
Ali Amori Kadhim <ali@frameless.io>
2024-06-04 22:12:26 +0200

    feat: improve TOC component

--- components/reusable/TOC/index.tsx
+++ components/reusable/TOC/index.tsx
@@ -4,11 +4,12 @@ import slugify from 'slugify';
 import Link from 'next/link';
 
 import { HeadingsArray } from 'global';
+import { useWindowSize } from '@/utils';
 
 import { List } from '../List';
 import { ListItem } from '../ListItem';
-import { Heading } from '../Heading';
 import { Anchor } from '../Anchor';
+import { Details } from '../Details';
 
 import styles from './styles.module.scss';
 
@@ -20,84 +21,83 @@ const cx = classnames.bind(styles);
 export const TOC = forwardRef(
   ({ toc, label, ...restProps }: PropsWithChildren<TOCProps>, ref: ForwardedRef<HTMLElement>) => {
     const isToc = toc && Array.isArray(toc);
+    const { width } = useWindowSize();
+    const smallScreen = width ? width < 960 : false;
 
     return isToc ? (
-      <nav ref={ref} className={cx('toc')} {...restProps}>
-        {label && (
-          <Heading variant="h2" level={2}>
-            {label}
-          </Heading>
-        )}
-        <List className={cx('toc__list')}>
-          {toc.map((item) => (
-            <ListItem key={item.id}>
-              <Anchor Link={Link} href={`#${slugify(item.textContent, { lower: true })}`} className={cx('toc__link')}>
-                {item.textContent}
-              </Anchor>
-              {item.nestedH3 && (
-                <List className={cx('toc__list-nested')}>
-                  {item.nestedH3.map((h3) => (
-                    <ListItem key={h3?.id}>
-                      <Anchor
-                        Link={Link}
-                        href={`#${slugify(h3.textContent, { lower: true })}`}
-                        className={cx('toc__link')}
-                      >
-                        {h3.textContent}
-                      </Anchor>
-                      {h3.nestedH4 && (
-                        <List className={cx('toc__list-nested')}>
-                          {h3.nestedH4.map((h4) => (
-                            <ListItem key={h4.id}>
-                              <Anchor
-                                Link={Link}
-                                href={`#${slugify(h4.textContent, { lower: true })}`}
-                                className={cx('toc__link')}
-                              >
-                                {h4.textContent}
-                              </Anchor>
-                              {h4.nestedH5 && (
-                                <List className={cx('toc__list-nested')}>
-                                  {h4.nestedH5.map((h5) => (
-                                    <ListItem key={h5.id}>
-                                      <Anchor
-                                        Link={Link}
-                                        href={`#${slugify(h5.textContent, { lower: true })}`}
-                                        className={cx('toc__link')}
-                                      >
-                                        {h5.textContent}
-                                      </Anchor>
-                                      {h5.nestedH6 && (
-                                        <List className={cx('toc__list-nested')}>
-                                          {h5.nestedH6.map((h6) => (
-                                            <ListItem key={h6.id}>
-                                              <Anchor
-                                                Link={Link}
-                                                href={`#${slugify(h6.textContent, { lower: true })}`}
-                                                className={cx('toc__link')}
-                                              >
-                                                {h6.textContent}
-                                              </Anchor>
-                                            </ListItem>
-                                          ))}
-                                        </List>
-                                      )}
-                                    </ListItem>
-                                  ))}
-                                </List>
-                              )}
-                            </ListItem>
-                          ))}
-                        </List>
-                      )}
-                    </ListItem>
-                  ))}
-                </List>
-              )}
-            </ListItem>
-          ))}
-        </List>
-      </nav>
+      <Details label={label} className={cx('toc')} open={!smallScreen} icon={smallScreen}>
+        <nav ref={ref} {...restProps}>
+          <List className={cx('toc__list')}>
+            {toc.map((item) => (
+              <ListItem key={item.id}>
+                <Anchor Link={Link} href={`#${slugify(item.textContent, { lower: true })}`} className={cx('toc__link')}>
+                  {item.textContent}
+                </Anchor>
+                {item.nestedH3 && (
+                  <List className={cx('toc__list-nested')}>
+                    {item.nestedH3.map((h3) => (
+                      <ListItem key={h3?.id}>
+                        <Anchor
+                          Link={Link}
+                          href={`#${slugify(h3.textContent, { lower: true })}`}
+                          className={cx('toc__link')}
+                        >
+                          {h3.textContent}
+                        </Anchor>
+                        {h3.nestedH4 && (
+                          <List className={cx('toc__list-nested')}>
+                            {h3.nestedH4.map((h4) => (
+                              <ListItem key={h4.id}>
+                                <Anchor
+                                  Link={Link}
+                                  href={`#${slugify(h4.textContent, { lower: true })}`}
+                                  className={cx('toc__link')}
+                                >
+                                  {h4.textContent}
+                                </Anchor>
+                                {h4.nestedH5 && (
+                                  <List className={cx('toc__list-nested')}>
+                                    {h4.nestedH5.map((h5) => (
+                                      <ListItem key={h5.id}>
+                                        <Anchor
+                                          Link={Link}
+                                          href={`#${slugify(h5.textContent, { lower: true })}`}
+                                          className={cx('toc__link')}
+                                        >
+                                          {h5.textContent}
+                                        </Anchor>
+                                        {h5.nestedH6 && (
+                                          <List className={cx('toc__list-nested')}>
+                                            {h5.nestedH6.map((h6) => (
+                                              <ListItem key={h6.id}>
+                                                <Anchor
+                                                  Link={Link}
+                                                  href={`#${slugify(h6.textContent, { lower: true })}`}
+                                                  className={cx('toc__link')}
+                                                >
+                                                  {h6.textContent}
+                                                </Anchor>
+                                              </ListItem>
+                                            ))}
+                                          </List>
+                                        )}
+                                      </ListItem>
+                                    ))}
+                                  </List>
+                                )}
+                              </ListItem>
+                            ))}
+                          </List>
+                        )}
+                      </ListItem>
+                    ))}
+                  </List>
+                )}
+              </ListItem>
+            ))}
+          </List>
+        </nav>
+      </Details>
     ) : null;
   },
 );
