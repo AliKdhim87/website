4adf275822888921e754761fcc048bf3c042110f
Ali Amori Kadhim <ali@frameless.io>
2024-06-08 21:00:29 +0200

    feat: improve socialMedia component

--- components/slices/socialMedia/index.tsx
+++ components/slices/socialMedia/index.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames/bind';
 import { Maybe } from 'graphql/jsutils/Maybe';
+import { AnchorHTMLAttributes, DetailedHTMLProps, ForwardedRef, PropsWithChildren, forwardRef } from 'react';
 
-import { Container, Grid, Icon, IconsType, Heading } from '@/components/reusable';
+import { Grid, Icon, IconsType, Heading } from '@/components/reusable';
 import { uuidv4 } from '@/utils';
 import { Social } from '@/graphql-types';
+import type { HeadingProps } from '@/components/reusable/Heading';
 
 import styles from './SocialMedia.module.scss';
 
@@ -14,39 +16,64 @@ interface SocialMediaProps {
   socialMedia: Maybe<Maybe<Social>[]>;
 }
 
-export const SocialMedia = ({ title, socialMedia }: SocialMediaProps) => (
-  <section className={css('social-media')}>
-    <Container>
-      <Grid container alignItems="center">
-        <Grid item sm={6} justifyContent="center" justifyContentMd="flex-start">
-          {title && (
-            <Heading level={2} className={css('social-media__title')}>
-              {title}
-            </Heading>
-          )}
-        </Grid>
-        <Grid item sm={3} justifyContent="center">
-          {socialMedia &&
-            socialMedia.map(
-              (item) =>
-                item &&
-                item.title &&
-                item.url &&
-                item.icon && (
-                  <a
-                    href={item.url}
-                    key={uuidv4()}
-                    title={item.title}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={css('social-media__link')}
-                  >
-                    <Icon name={item.icon as keyof typeof IconsType} aria-label={title} />
-                  </a>
-                ),
-            )}
-        </Grid>
-      </Grid>
-    </Container>
-  </section>
+export type SocialMediaTitleProps = Omit<HeadingProps, 'level'>;
+
+export const SocialMediaTitle = forwardRef(
+  ({ children, ...restProps }: PropsWithChildren<SocialMediaTitleProps>, ref: ForwardedRef<HTMLHeadingElement>) => (
+    <Heading
+      {...restProps}
+      ref={ref}
+      id="social-media-title"
+      variant="h4"
+      level={2}
+      className={css('social-media__title')}
+    >
+      {children}
+    </Heading>
+  ),
 );
+SocialMediaTitle.displayName = 'SocialMediaTitle';
+
+export type SocialMediaLinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
+  title?: string;
+  icon?: keyof typeof IconsType;
+};
+export const SocialMediaLink = forwardRef(
+  ({ icon, ...restProps }: PropsWithChildren<SocialMediaLinkProps>, ref: ForwardedRef<HTMLAnchorElement>) => (
+    <a ref={ref} {...restProps} className={css('social-media__link')} rel="noopener noreferrer" target="_blank">
+      {icon && <Icon role="presentation" name={icon as keyof typeof IconsType} />}
+    </a>
+  ),
+);
+SocialMediaLink.displayName = 'SocialMediaLink';
+SocialMediaLink.defaultProps = {
+  title: undefined,
+  icon: undefined,
+};
+
+export const SocialMedia = forwardRef(({ title, socialMedia }: SocialMediaProps) => (
+  <section className={css('social-media')} aria-labelledby="social-media-title">
+    <Grid container alignItems="center" justifyContent="space-between">
+      <Grid item sm={6} justifyContent="center" justifyContentMd="flex-start">
+        {title && <SocialMediaTitle>{title}</SocialMediaTitle>}
+      </Grid>
+      <Grid item sm={3} justifyContent="center" justifyContentMd="flex-end">
+        {Array.isArray(socialMedia) &&
+          socialMedia.map(
+            (item) =>
+              item &&
+              item?.url && (
+                <SocialMediaLink
+                  href={item.url}
+                  key={uuidv4()}
+                  title={item?.title || undefined}
+                  aria-label={item?.title || undefined}
+                  icon={item.icon as keyof typeof IconsType}
+                />
+              ),
+          )}
+      </Grid>
+    </Grid>
+  </section>
+));
+SocialMedia.displayName = 'SocialMedia';
