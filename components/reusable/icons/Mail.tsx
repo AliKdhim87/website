cb1301b4d6421c82f48378250fe5236c68dbf6eb
Ali Amori Kadhim <ali@frameless.io>
2024-06-08 21:08:14 +0200

    fixup! feat: improve social media SVG icons

--- components/reusable/icons/Mail.tsx
+++ components/reusable/icons/Mail.tsx
@@ -10,18 +10,16 @@ const SvgMail = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRPro
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    fill="none"
-    viewBox="0 0 31 30"
+    viewBox="0 0 15 15"
     className="icon"
     aria-labelledby={titleId}
     {...props}
   >
     {title ? <title id={titleId}>{title}</title> : null}
     <path
-      fill="currentColor"
-      fillRule="evenodd"
-      d="M15.5 14.823 3.505 8.826A3 3 0 0 1 6.5 6h18a3 3 0 0 1 2.996 2.826zm0 3.354 12-6V21a3 3 0 0 1-3 3h-18a3 3 0 0 1-3-3v-8.823z"
-      clipRule="evenodd"
+      fill="none"
+      stroke="currentColor"
+      d="m.5 4.5 7 4 7-4m-13-3h12a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-12a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1Z"
     />
   </svg>
 );
