46751f10fafeba1a598bfbb364e2c3d344e448a3
Ali Amori Kadhim <ali@frameless.io>
2024-06-30 13:46:10 +0200

    refactor: refactor `<PageHeader />` component

--- components/slices/PageHeader/index.tsx
+++ components/slices/PageHeader/index.tsx
@@ -1,84 +1,48 @@
+import type { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
+
 import classNames from 'classnames/bind';
 import { Maybe } from 'graphql/jsutils/Maybe';
 import Image, { ImageProps } from 'next/image';
 
-import { Container, Typography, Grid, CTA, Heading } from '@/components/reusable';
-import { Cta } from '@/graphql-types';
+import { Container, Typography, Grid, Heading } from '@/components/reusable';
 
 import styles from './PageHeader.module.scss';
 
 const css = classNames.bind(styles);
 
-export type PageHeader = {
+export interface PageHeaderProps extends Omit<DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>, 'title'> {
   title?: Maybe<string>;
   body?: Maybe<string>;
   image?: ImageProps;
-  titleDistancedBottom?: boolean;
-  cta?: Maybe<Cta>;
-  backgroundColor?: 'primary' | 'transparent';
-};
+}
 
-export const PageHeader = ({
-  title,
-  body,
-  image,
-  titleDistancedBottom,
-  cta,
-  backgroundColor = 'primary',
-}: PageHeader) => {
-  const content = (
-    <>
-      {title && (
-        <Heading level={1} className={css({ 'space-mb-end-3': titleDistancedBottom })}>
-          {title}
-        </Heading>
-      )}
-      {body && (
-        <Typography as="span" variant="subtitle">
-          {body}
-        </Typography>
-      )}
-      {cta && cta.route && cta.title && (
-        <CTA href={cta.route} className={css('space-mb-start-4', 'space-auto')}>
-          {cta.title}
-        </CTA>
-      )}
-    </>
-  );
-
-  const rootClasses = css('page-header-section', { [`page-header-section--bg-${backgroundColor}`]: backgroundColor });
+export const PageHeader = ({ title, body, image, children }: PropsWithChildren<PageHeaderProps>) => {
+  const rootClasses = css('page-header-section');
   return (
     <section className={rootClasses}>
       <Container>
-        <Grid container justifyContent="center">
-          <Grid item md={10}>
-            <Grid container>
-              {image ? (
-                <>
-                  <Grid item md={8} sm={6}>
-                    {content}
-                  </Grid>
-                  {image.src && image.alt && (
-                    <Grid
-                      item
-                      md={4}
-                      sm={6}
-                      xs={12}
-                      justifyContent="center"
-                      className={css('page-header-section__image')}
-                    >
-                      <Image src={image.src} width={image.width} height={image.height} alt={image.alt} />
-                    </Grid>
-                  )}
-                </>
-              ) : (
-                <Grid item md={12}>
-                  {content}
-                </Grid>
+        <Grid container spacing={image ? 'md' : undefined}>
+          <Grid item md={image ? 9 : undefined}>
+            <hgroup className={css('page-header__heading-group')}>
+              {title && (
+                <Heading level={1} className={css('page-header__title')}>
+                  {title}
+                </Heading>
               )}
-            </Grid>
+              {body && (
+                <Typography variant="subtitle" className={css('page-header__subtitle')}>
+                  {body}
+                </Typography>
+              )}
+            </hgroup>
           </Grid>
+          {image && image.src && image.alt && (
+            <Grid item md={3} justifyContent="center">
+              <Image src={image.src} width={image.width} height={image.height} alt={image.alt} />
+            </Grid>
+          )}
         </Grid>
+        {children}
       </Container>
     </section>
   );
