3056976f1b5bc5391beba1655c0574bb79189a04
Ali Amori Kadhim <ali@frameless.io>
2024-07-01 00:19:41 +0200

    feat: improve the PostList component

--- components/slices/PostList/index.tsx
+++ components/slices/PostList/index.tsx
@@ -3,7 +3,7 @@ import { ForwardedRef, forwardRef } from 'react';
 import classnames from 'classnames/bind';
 import Link from 'next/link';
 
-import { Container, Grid, Card, CardProps, CTA, Heading } from '@/components/reusable';
+import { Card, CardProps, CTA, Heading } from '@/components/reusable';
 import { Cta, Maybe } from '@/graphql-types';
 import { uuidv4 } from '@/utils';
 
@@ -11,7 +11,7 @@ import styles from './styles.module.scss';
 
 const cx = classnames.bind(styles);
 
-export interface PostItemProps extends Readonly<CardProps> {
+export interface PostListItemProps extends Readonly<CardProps> {
   href: string;
   title: string;
   body: string;
@@ -25,53 +25,47 @@ export type BlogType = {
   publishedAt: string;
 };
 
-export interface PostListProps extends Pick<PostItemProps, 'headingOptions'> {
+export interface PostListProps extends Pick<PostListItemProps, 'headingOptions'> {
   title?: Maybe<string>;
   blog?: BlogType[];
   cta?: Maybe<Cta>;
 }
 
-export const PostItem = forwardRef(
-  ({ body, href, publishedAt, title, ...restProps }: PostItemProps, ref: ForwardedRef<HTMLAnchorElement>) => (
-    <Link ref={ref} href={href} className={cx('post__item')}>
+export const PostListItem = forwardRef(
+  ({ body, href, publishedAt, title, ...restProps }: PostListItemProps, ref: ForwardedRef<HTMLAnchorElement>) => (
+    <Link ref={ref} href={href} className={cx('post-list__item')}>
       <Card title={title} body={body} publishedAt={publishedAt} {...restProps} />
     </Link>
   ),
 );
 
-PostItem.displayName = 'PostItem';
+PostListItem.displayName = 'PostListItem';
 
 export const PostList = ({ blog, cta, title, ...restProps }: PostListProps) => (
-  <section className="space-pb-4">
-    <Container>
-      <Grid container justifyContent="center">
-        <Grid md={10}>
-          {title && (
-            <Heading level={2} variant="h3" className="text-align-md--center">
-              {title}
-            </Heading>
-          )}
-          {blog &&
-            blog.map(
-              ({ slug, title: blogTitle, excerpt, publishedAt }) =>
-                slug.current && (
-                  <PostItem
-                    {...restProps}
-                    key={uuidv4()}
-                    title={blogTitle}
-                    body={excerpt}
-                    publishedAt={publishedAt}
-                    href={`/blog/${slug.current}`}
-                  />
-                ),
-            )}
-          {cta && cta.route && (
-            <CTA href={cta.route} className="space-mb-start-5">
-              {cta.title}
-            </CTA>
-          )}
-        </Grid>
-      </Grid>
-    </Container>
+  <section className={cx('post-list')}>
+    {title && (
+      <Heading level={2} variant="h3" className="post-list__title">
+        {title}
+      </Heading>
+    )}
+    {Array.isArray(blog) &&
+      blog.map(
+        ({ slug, title: blogTitle, excerpt, publishedAt }) =>
+          slug.current && (
+            <PostListItem
+              {...restProps}
+              key={uuidv4()}
+              title={blogTitle}
+              body={excerpt}
+              publishedAt={publishedAt}
+              href={`/blog/${slug.current}`}
+            />
+          ),
+      )}
+    {cta && cta.route && (
+      <CTA href={cta.route} className={cx('post-list__cta')}>
+        {cta.title}
+      </CTA>
+    )}
   </section>
 );
