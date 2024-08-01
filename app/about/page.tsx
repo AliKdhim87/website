19ebe211ae43d1a934bb14dda5e58d8038d152ce
Ali Amori Kadhim <ali@frameless.io>
2024-08-01 20:02:07 +0200

    fix: update canonical url

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -28,6 +28,9 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: openGraph?.title,
     description: openGraph?.description,
+    alternates: {
+      canonical: '/about',
+    },
     openGraph: {
       images: openGraph?.image?.asset?.url || undefined,
       description: openGraph?.description || undefined,
