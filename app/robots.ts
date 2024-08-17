7b156075c8de688017a1d95108a7cdfd5c16932d
Ali Amori Kadhim <ali@frameless.io>
2024-08-18 00:34:48 +0200

    fix: add more disallowed paths

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/api/', '/_next/', '/404', '/500', '/public/'],
+      },
+    ],
     host: process.env.SITE_URL,
     sitemap: `${process.env.SITE_URL}/sitemap.xml`,
   };
