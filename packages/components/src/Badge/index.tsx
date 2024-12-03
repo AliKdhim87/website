52ca516ac7208781368fdc190e502f23211b51cf
Ali Amori Kadhim <ali@frameless.io>
2024-12-03 22:34:15 +0100

    feat(components): add Link property to Badge component

--- packages/components/src/Badge/index.tsx
+++ packages/components/src/Badge/index.tsx
@@ -1,20 +1,21 @@
 import { forwardRef } from 'react';
-import type { HTMLAttributes, PropsWithChildren, ForwardedRef } from 'react';
+import type { PropsWithChildren, ForwardedRef, ComponentType, AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
 import classNames from 'classnames/bind';
-import Link, { LinkProps } from 'next/link';
 
 import styles from './Badge.module.scss';
 
 const css = classNames.bind(styles);
 
-interface BadgeProps extends LinkProps, HTMLAttributes<HTMLAnchorElement> {}
+interface BadgeProps extends DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+  Link?: ComponentType<any>;
+}
 
-export const Badge = forwardRef(
-  ({ children, ...restProps }: PropsWithChildren<BadgeProps>, ref: ForwardedRef<HTMLAnchorElement>) => (
-    <Link {...restProps} className={css('ali-dev-badge', restProps.className)} ref={ref}>
+export const Badge = forwardRef<HTMLAnchorElement, PropsWithChildren<BadgeProps>>(
+  ({ children, Link: Component = 'a', className, ...restProps }, ref: ForwardedRef<HTMLAnchorElement>) => (
+    <Component {...restProps} className={css('ali-dev-badge', className)} ref={ref}>
       {children}
-    </Link>
+    </Component>
   ),
 );
 Badge.displayName = 'Badge';
